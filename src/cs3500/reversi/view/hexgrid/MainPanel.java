ad9810d1919dffa18b2bc450758dea790fb30f0a
aarushisurve <surve.aa@northeastern.edu>
2023-12-13 00:04:46 -0600

    read me

--- src/cs3500/reversi/view/hexgrid/MainPanel.java
+++ src/cs3500/reversi/view/hexgrid/MainPanel.java
@@ -1,7 +1,5 @@
 package cs3500.reversi.view.hexgrid;
 
-import cs3500.reversi.model.BoardType;
-import cs3500.reversi.model.Cell;
 import cs3500.reversi.model.CellState;
 import cs3500.reversi.model.ReadonlyReversiModel;
 import java.awt.BasicStroke;
@@ -159,11 +157,7 @@ public class MainPanel extends JPanel {
     metrics = g.getFontMetrics();
 
     drawRectangle(g2d, origin, 380, true, true, Color.BLACK, 0);
-    if (model.getBoardType().equals(BoardType.HEXAGON)) {
-      drawHexGridLoop(g2d, origin, model.getRows(), 50, 1);
-    } else {
-      drawSquareGridLoop(g2d, origin, model.getRows(), true,50, 1);
-    }
+    drawHexGridLoop(g2d, origin, model.getRows(), 50, 1);
     drawStatusMessages(g2d, origin, 380, true, Color.WHITE);
     drawPassTurnButton(g2d, origin, 380, true, Color.LIGHT_GRAY);
     drawRestartButton(g2d, origin, 380, true, Color.LIGHT_GRAY);
@@ -264,59 +258,6 @@ public class MainPanel extends JPanel {
     }
   }
 
-
-  // draws the squaregrid loop
-  private void drawSquareGridLoop(Graphics g, Point origin, int size, boolean centered, int radius, int padding) {
-    board = model.getGameBoard();
-
-    int width = origin.x - radius;
-    int height = origin. y - radius;
-
-    int cellWidth = width / size;
-    int cellHeight = height / size;
-
-    int xOffset = (width - (size * cellWidth)) / 2;
-    int yOffset = (height - (size * cellHeight)) / 2;
-
-    double ang30 = Math.toRadians(30);
-    double xOff = Math.cos(ang30) * (radius + padding);
-    double yOff = Math.sin(ang30) * (radius + padding);
-    int half = size / 2;
-
-    POINTS_TO_ROW_COLS = new HashMap<>();
-    Color defaultValue;
-    for (int row = 0; row < size; row++) {
-      for (int col = 0; col < size; col++) {
-        int xLbl;
-        int yLbl;
-        xLbl = row;
-        yLbl = col;
-        int spacing = 40;
-        int x =  (int) (origin.x - width/2 + spacing + xOffset + (col * cellWidth));
-        int y = (int) (origin.y - height/2 + + spacing + yOffset + (row * cellHeight));
-        POINTS_TO_ROW_COLS.put(new Point(x, y), new Point(row, col));
-        defaultValue = null;
-        String text = null;
-        Color hightlightColor = null;
-        if (board.get(row).get(col) == CellState.EMPTY) {
-          if (this.currentRow == row && this.currentCol == col) {
-            //System.out.println("currentRow " + row + " currentCol " + col);
-            hightlightColor = Color.CYAN;
-            if (defaultValue == null) {
-              text = this.hints;
-              //System.out.println("hints text " + text);
-            }
-          }
-        } else if (board.get(row).get(col) == CellState.BLACK) {
-          defaultValue = Color.BLACK;
-        } else if (board.get(row).get(col) == CellState.WHITE) {
-          defaultValue = Color.WHITE;
-        }
-        drawSquare(g, xLbl, yLbl, x, y, cellWidth, cellHeight, radius, centered, defaultValue, hightlightColor, text);
-      }
-    }
-  }
-
   // draws the hexgrid loop
   private void drawHexGridLoop(Graphics g, Point origin, int size, int radius, int padding) {
     board = model.getGameBoard();
@@ -397,41 +338,6 @@ public class MainPanel extends JPanel {
 
   }
 
-
-  // draw the square
-  private void drawSquare(Graphics g, int posX, int posY, int x, int y, int cellWidth, int cellHeight, int r, boolean centered, Color colorValue,
-                       Color hightlightColor, String text) {
-    Graphics2D g2d = (Graphics2D) g;
-
-    if (hightlightColor != null) {
-      drawRect(g2d, x, y, cellWidth, cellHeight, r, centered, true, hightlightColor, 0);
-    } else {
-      drawRect(g2d, x, y, cellWidth, cellHeight, r, centered, true, Color.GRAY, 0);
-    }
-    drawRect(g2d, x, y, cellWidth, cellHeight, r, centered, false, Color.DARK_GRAY, 4);
-
-    if (colorValue != null) {
-      g.setColor(colorValue);
-      text = String.format("%s : %s", coord(posX), coord(posY));
-      int w = metrics.stringWidth(text);
-      int h = metrics.getHeight();
-      int spacing = w;
-      if (posX == 0 || posY == 0) {
-        spacing += 10;
-      }
-      g.fillOval(x - spacing, y - spacing, cellWidth-20, cellWidth-20);
-    } else {
-      //System.out.println("Hints "+text);
-      if (text != null) {
-        int w = metrics.stringWidth(text);
-        int h = metrics.getHeight();
-        g.setColor(Color.BLACK);
-        g.drawString(text, x - w / 2, y + h / 2);
-      }
-    }
-
-  }
-
   // coordinate value of a point
   private String coord(int value) {
     return (value > 0 ? "+" : "") + value;
@@ -459,34 +365,6 @@ public class MainPanel extends JPanel {
     }
     else {
       g.drawRect(x2, y2, diameter, diameter);
-    }
-
-    // Set values to previous when done.
-    g.setColor(tmpC);
-    g.setStroke(tmpS);
-  }
-
-  /**
-   * Draws the rectangle with width and height.
-   */
-  public void drawRect(Graphics2D g, int x, int y, int width, int height, int radius,
-                            boolean centered, boolean filled, Color colorValue, int lineThickness) {
-    // Store before changing.
-    Stroke tmpS = g.getStroke();
-    Color tmpC = g.getColor();
-
-    g.setColor(colorValue);
-    g.setStroke(new BasicStroke(lineThickness, BasicStroke.CAP_ROUND,
-            BasicStroke.JOIN_ROUND));
-
-    int x2 = centered ? x - radius : x;
-    int y2 = centered ? y - radius : y;
-
-    if (filled) {
-      g.fillRect(x2, y2, width, height);
-    }
-    else {
-      g.drawRect(x2, y2, width, height);
     }
 
     // Set values to previous when done.
